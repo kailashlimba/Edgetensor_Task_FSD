9acce0fba57eb354cf49513a39b4d3f7c5f282e7
Kailash Limba <kailash@culturro.com>
2020-03-28 21:03:38 +0530

    grayscale condition added

--- Edge.py
+++ Edge.py
@@ -2,7 +2,7 @@ import cv2
 import time
 # frequency in seconds
 
-def capture_image(frequency):
+def capture_image(frequency,grayscale):
     img_counter = 0
     while True:
         cam = cv2.VideoCapture(0)
@@ -13,8 +13,15 @@ def capture_image(frequency):
         cv2.destroyAllWindows()
         img_name = "opencv_frame_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
+        image = cv2.imread(img_name)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('Original image',image)
+        # cv2.imshow('Gray image', gray)
+        if grayscale:
+            cv2.imwrite(img_name, gray)
+
         print("{} written!".format(img_name))
         img_counter += 1
         time.sleep(5)
 
-capture_image(5)
+capture_image(5,1)
