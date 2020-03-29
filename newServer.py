a77392fd25d35eafb716a4742cfa803394e12f9a
Kailash Limba <kailash@culturro.com>
2020-03-29 17:44:07 +0530

    communication modified

--- newServer.py
+++ newServer.py
@@ -1,5 +1,7 @@
 
-from flask import Flask
+from flask import Flask, request, Response
+import numpy as np
+import jsonpickle
 app = Flask(__name__)
 import requests as req
 
@@ -11,8 +13,24 @@ def capture():
         req.get(url)
         time.sleep(2)
 
-    
-    
+@app.route("/store",methods=['POST'])
+def store():
+    r = request
+    # convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # do some fancy processing here....
+
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
+
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=90)
