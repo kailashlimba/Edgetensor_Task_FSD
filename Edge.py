95f0241191f2a5974988741f607ddc2eec9569fb
Kailash Limba <kailash@culturro.com>
2020-03-29 17:43:34 +0530

    communication modified

--- Edge.py
+++ Edge.py
@@ -1,14 +1,19 @@
 import cv2
 import time
 from flask import Flask
+import requests as req
 
-app = Flask(__name__)
+# app = Flask(__name__)
 # frequency in seconds
-# capture_image(5,1)
+
 
 url = 'http://localhost:90/capture'
 
-@app.route("/Edge/<string:frequency>/<string:grayscale>")
+# prepare headers for http request
+content_type = 'image/jpeg'
+headers = {'content-type': content_type}
+
+# @app.route("/Edge/<string:frequency>/<string:grayscale>")
 def capture_image(frequency,grayscale):
     img_counter = 0
     while True:
@@ -31,8 +36,15 @@ def capture_image(frequency,grayscale):
         print("{} written!".format(img_name))
         img_counter += 1
 
-        # req.post(url,frame)
+        req.post(url,image)
+
+        _, img_encoded = cv2.imencode('.jpg', image)
+        # send http request with image and receive response
+        response = req.post(url, data=img_encoded.tostring(), headers=headers)
+        print(json.loads(response.text))
         time.sleep(int(frequency))
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+
+capture_image(5,1)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80)
